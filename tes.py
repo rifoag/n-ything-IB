02ffdd69071b8eca3f134ab9134d4898568ca5c9
Krishna Aurelio N <krishna.aurelio@yahoo.com>
2018-09-16 13:13:02 +0700

    Kerjain boiss

--- tes.py
+++ tes.py
@@ -18,10 +18,10 @@ def createPion(dataPion,pions):
 
   x = random.randrange(8)
   y = random.randrange(8)
-  listPoint.append((x,y))
+
 
   for i in range(0,amount):
-    while( (x,y) in listPoint):
+    while((x,y) in listPoint):
       x = random.randrange(8)
       y = random.randrange(8)
     listPoint.append((x,y))
@@ -36,9 +36,20 @@ def printAllPion(pions):
     print("PositionY: ", pion['col'], "\n")
 
 # Print chess board
-def printBoard():
+def printBoard(pions):
   for i in range(0,8):
     for j in range(0,8):
+      for pion in pions:
+        if (pion['row']==i and pion['col']==j):
+          if (pion['Type']=='QUEEN'):
+            print('Q',end="")
+          elif (pion['Type']=='BISHOP'):
+            print('B',end="")
+          elif (pion['Type']=='ROOK'):
+            print('R',end="")
+          elif (pion['Type']=='KNIGHT'):
+            print('K',end="")
+      else:
         print("-",end="")
     print("\n")
 
@@ -50,11 +61,27 @@ def checkAttack(currPion,adjPion):
     adjY = adjPion["col"]
 
     if (currPion['Type'] == "QUEEN"):
+      return currX==adjX or currY==adjY or abs(currX - currY) == abs (adjX - adjY) 
     if (currPion['Type'] == "KNIGHT"):
+      return (abs(currX-adjX)==2 and abs(currY-adjY)==1) or (abs(currX-adjX)==1 and abs(currY-adjY)==2)
     if (currPion['Type'] == "BISHOP"):
+      return abs(currX - currY) == abs (adjX - adjY)
     if (currPion['Type'] == "ROOK"):
+      return currX==adjX or currY==adjY
+
+# fungsi Evaluasi jumlah penyerangan      
+def Eval(pions):
+  sumAtk=0
+  for pion in pions:
+    # temp = [x for x in pions if x!=pion]
+    # for adj in temp:
+    for adj in pions:
+      if checkAttack(pion,adj) and pion!=adj:
+        sumAtk+=1
+  return sumAtk
 
 pions = []
 readFile('input.txt',pions)
 printAllPion(pions)
-printBoard()
+printBoard(pions)
+print(Eval(pions))
