cb6fc67e10ac3b48d779e9f4ac4efb2dbefe9fe2
dgdp16 <gantenggantengcoder@gmail.com>
2018-09-16 11:05:55 +0700

    Add data structure

---
+++ tes.py
@@ -0,0 +1,28 @@
+# Read pions data from file external
+def readFile(fileName, pions):
+    file = open(fileName, 'r')
+    data = file.read()
+    data = data.split('\n') # List of entire data
+    dataSplited = []
+    for x in data:
+        dataSplited.append(x.split(' '))
+    for y in dataSplited:
+        createPion(y,pions)
+
+# Create pions' data to dictionary
+def createPion(dataPion,pions):
+    amount = int(dataPion[2])
+    for i in range(0,amount):
+        pions.append({'Type' : dataPion[1], 'color' : dataPion[0], 'row': -1, 'col' : -1})
+
+# Print All Pions' data (Type, Color, Position)
+def printAllPion(pions):
+    for pion in pions:
+        print("Type: ", pion['Type'])
+        print("Color: ", pion['color'])
+        print("PositionX: ", pion['row'])
+        print("PositionY: ", pion['col'], "\n")
+
+pions = []
+readFile('input.txt',pions)
+printAllPion(pions)
