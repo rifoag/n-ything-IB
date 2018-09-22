c0c0cc2f2e271e232a42dca65861f8f21c3bcc5d
Krishna Aurelio N <krishna.aurelio@yahoo.com>
2018-09-22 18:46:16 +0700

    Simulated Annealing

--- nything.py
+++ nything.py
@@ -1,5 +1,6 @@
 import random
 import copy
+from math import exp
 
 # Read pawns data from file external
 def readFile(fileName, pawns):
@@ -169,6 +170,35 @@ def hillClimbing(initState):
         evalCurrent = evaluate(current)
   return current
 
+def decision(probability):
+  return random.randrange(100) < probability
+
+# Simulated Annealing function, temperature decreasing by ratio
+def simulatedAnnealing(initState,temperature,decreaseRate,iteration):
+  current = initState
+  evalCurrent = evaluate(current)
+  i = 0
+  isOver = False
+  while (evalCurrent != 0 and i < iteration and not isOver):
+    isOver = True
+    AllNeighbour = listAllNeighbour(current)
+    for neighbour in AllNeighbour:
+      if (evalCurrent > evaluate(neighbour)):
+        current = neighbour
+        evalCurrent = evaluate(current)
+        i+=1
+        temperature *= decreaseRate/100
+        isOver = False
+      else:
+        probability = exp(evaluate(neighbour)-evalCurrent/temperature)
+        if (decision(probability)):
+          current = neighbour
+          evalCurrent = evaluate(current)
+          i+=1
+          temperature *= decreaseRate/100
+          isOver = False
+  return current
+
 pawns = []
 readFile('input.txt',pawns)
 printAllPawns(pawns)
@@ -177,3 +207,7 @@ print(evaluate(pawns))
 allNeighbour = listAllNeighbour(pawns)
 printBoard(hillClimbing(pawns))
 print(evaluate(hillClimbing(pawns)))
+hasil = simulatedAnnealing(pawns,100,80,1000)
+printBoard(hasil)
+print(evaluate(hasil))
+
