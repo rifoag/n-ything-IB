e8174c9039032d002f1a987dcc9408864ffb3c49
dafifaraz <30387504+dafifaraz@users.noreply.github.com>
2018-09-29 17:33:29 +0700

    Fixed crossOver procedure

--- nything.py
+++ nything.py
@@ -226,25 +226,25 @@ def createListOfPawns(dataSplitted,numberOfPawns,JumlahPawns):
   return listOfPawns
 
 # crossover setengah dari anak
-def crossOver(Population):
-    #iteration here
-    anak1 = []
-    anak2 = []
-    crossIteration = len(Population[0])/2
-    print(crossIteration)
-    if (len(Population[0]) % 2 == 0):
-        anak1.append(Population[0][0:int(len(Population[0])/2)])
-        anak1.append(Population[1][int(len(Population[0]) / 2 + 1):len(Population[1])])
-        anak2.append(Population[0][0:int(len(Population[0]) / 2)])
-        anak2.append(Population[1][int(len(Population[0]) / 2 + 1):len(Population[1])])
-    else:
-        anak1.append(Population[0][0:int(len(Population[0]) / 2)])
-        anak1.append(Population[1][int(len(Population[0]) / 2 + 2):len(Population[1])])
-        anak2.append(Population[0][0:int(len(Population[0]) / 2)])
-        anak2.append(Population[1][int(len(Population[0]) / 2 + 2):len(Population[1])])
-    Population[0] = anak1
-    Population[1] = anak2
-    ####################
+def crossOver(popSize, Population):
+    print(len(Population))
+    for n in range(int(popSize/2)):
+      anak1 = []
+      anak2 = []
+      crossIteration = len(Population[0])/2
+      print(crossIteration)
+      if (len(Population[0]) % 2 == 0):
+          anak1.append(Population[n][0:int(len(Population[0])/2)])
+          anak1.append(Population[n+1][int(len(Population[0]) / 2 + 1):len(Population[n])])
+          anak2.append(Population[n][0:int(len(Population[0]) / 2)])
+          anak2.append(Population[n+1][int(len(Population[0]) / 2 + 1):len(Population[n])])
+      else:
+          anak1.append(Population[n][0:int(len(Population[0]) / 2)])
+          anak1.append(Population[n+1][int(len(Population[0]) / 2 + 2):len(Population[n])])
+          anak2.append(Population[n][0:int(len(Population[0]) / 2)])
+          anak2.append(Population[n+1][int(len(Population[0]) / 2 + 2):len(Population[n])])
+      Population[n] = anak1
+      Population[n+1] = anak2
     
 # Mengganti posisi pion secara random ke posisi random
 def mutation(Population, mutationFactor):
@@ -276,7 +276,7 @@ def geneticAlgorithm(pop_size,gen_amount,dataSplitted,numberOfPawns):
     print(Population)
     for x in range(0,gen_amount):
         Population = fitness(Population,numberOfPawns)
-        crossOver(Population)
+        crossOver(popSize, Population)
         mutation(Population,50)
         for y in Population:
             if(evaluate(y,numberOfPawns)==0):
