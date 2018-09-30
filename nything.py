4eaad5762a7cd6e842f5c4abb0bd5b92bea38266
Krishna Aurelio N <krishna.aurelio@yahoo.com>
2018-09-30 13:26:57 +0700

    rapihin dikit

--- nything.py
+++ nything.py
@@ -3,7 +3,7 @@ import copy
 from math import exp
 from operator import itemgetter
 
-# Read pawns data from file external
+# Read state data from file external
 def readFile(fileName, dataSplitted):
   file = open(fileName, 'r')
   data = file.read()
@@ -12,29 +12,33 @@ def readFile(fileName, dataSplitted):
     dataSplitted.append(row.split(' ')) # Parse input (per row) into a temporary array
 
 # membuat state dari data
-def parsePawns(dataSplitted,numberOfPawns):
-  pawns=[]
+def parseState(dataSplitted,pawnAmountBW):
+  state=[]
   listPoint=[]
   for row in dataSplitted:
-    createPawn(row, pawns, listPoint, numberOfPawns) # Parse into desired format
-  return pawns
+    createPawn(row, state, listPoint, pawnAmountBW) # Parse into desired format
+  return state
 
 # membuat list of state dari data
-def createListOfPawns(dataSplitted,numberOfPawns,JumlahPawns):
-  listOfPawns=[]
-  for i in range(0,JumlahPawns):
-    listOfPawns.append(parsePawns(dataSplitted,numberOfPawns))
-  return listOfPawns
+def createListOfState(dataSplitted,pawnAmountBW,jumlahState):
+  listOfState=[]
+  for i in range(0,jumlahState):
+    listOfState.append(parseState(dataSplitted,pawnAmountBW))
+  return listOfState
+
 # Menu
-def menuInit(pawns, numberOfPawns):
+def menuInit():
   fileName = input('Enter file name : ')
   dataSplitted=[]
   readFile(fileName, dataSplitted)
-  pawns=parsePawns(dataSplitted,numberOfPawns)
-  print("Pawns Data : ")
-  printAllPawns(pawns)
+  pawnAmountBW = {}
+  pawnAmountBW['WHITE'] = 0
+  pawnAmountBW['BLACK'] = 0
+  state=parseState(dataSplitted,pawnAmountBW)
+  print("State Data : ")
+  printAllState(state)
   print("Board First Condition : ")
-  printBoard(pawns)
+  printBoard(state)
   print("Choose Algorithm By Input The Number :")
   print("1. Hill Climbing")
   print("2. Simulted Annealing")
@@ -44,30 +48,23 @@ def menuInit(pawns, numberOfPawns):
     print("Chose The Correct Number Please...")
     chosenAlgo = int(input("Choose : "))
   if (chosenAlgo == 1):
-    hasil = hillClimbing(pawns,numberOfPawns)
-    print(hasil)
-    print(evaluate(hasil,numberOfPawns))
+    hasil = hillClimbing(state,pawnAmountBW)
   elif (chosenAlgo == 2):
     temperature = int(input('Temperature: '))
     decreaseRate = int(input('Decrease Rate: '))
     iteration = int(input('Maximum Iteration: '))
-    hasil = simulatedAnnealing2(pawns,numberOfPawns,temperature,decreaseRate,iteration)
-    print(hasil)
-    print(evaluate(hasil,numberOfPawns))
+    hasil = simulatedAnnealing(state,pawnAmountBW,temperature,decreaseRate,iteration)
   elif (chosenAlgo == 3):
     jumlahPopulasi= int(input('Sum Of Population: '))
     limit = int(input('Maximum generation: '))
-    listOfPawns = createListOfPawns(dataSplitted,numberOfPawns,jumlahPopulasi)
-    hasil = geneticAlgoritm(listOfPawns,numberOfPawns,jumlahPopulasi,limit)
-    print(len(hasil))
-    for state in hasil:
-      print("state: ")
-      print(state)
-      print(evaluate(state,numberOfPawns))
+    listOfstate = createListOfState(dataSplitted,pawnAmountBW,jumlahPopulasi)
+    hasil = geneticAlgoritm(listOfstate,pawnAmountBW,jumlahPopulasi,limit)
+  printBoard(hasil)
+  print(evaluate(hasil,pawnAmountBW))
 
-# Create pawns' data to dictionary
-def createPawn(datapawn, pawns, listPoint, numberOfPawns):
-  amount = int(datapawn[2])
+# Create state' data to dictionary
+def createPawn(dataPawn, state, listPoint, pawnAmountBW):
+  amount = int(dataPawn[2])
   x = random.randrange(8)
   y = random.randrange(8)
 
@@ -76,23 +73,23 @@ def createPawn(datapawn, pawns, listPoint, numberOfPawns):
       x = random.randrange(8)
       y = random.randrange(8)
     listPoint.append((x,y))
-    pawns.append({'type' : datapawn[1], 'color' : datapawn[0], 'row': x, 'col' : y})
-    numberOfPawns[datapawn[0]] += 1
+    state.append({'type' : dataPawn[1], 'color' : dataPawn[0], 'row': x, 'col' : y})
+    pawnAmountBW[dataPawn[0]] += 1
 
-# Print all pawns data (type, Color, Position)
-def printAllPawns(pawns):
-  for pawn in pawns:
+# Print all state data (type, Color, Position)
+def printAllState(state):
+  for pawn in state:
     print("type: ", pawn['type'])
     print("Color: ", pawn['color'])
     print("PositionX: ", pawn['row'])
     print("PositionY: ", pawn['col'], "\n")
 
 # Print chessboard
-def printBoard(pawns):
+def printBoard(state):
   for i in range(0,8):
     for j in range(0,8):
       isPawnExist = False
-      for pawn in pawns:
+      for pawn in state:
         if (pawn['row']==i and pawn['col']==j):
           isPawnExist = True
           if (pawn['color'] == 'BLACK'):
@@ -118,21 +115,21 @@ def printBoard(pawns):
     print("\n")
 
 # Check whether the current pawn can attack the other pawn
-def checkAttack(currPawn,dirPawn, pawns):
+def checkAttack(currPawn,dirPawn, state):
     if (currPawn['type'] == "QUEEN"):
-      return canAttackHorizontally(currPawn, dirPawn, pawns) or canAttackVertically(currPawn, dirPawn, pawns) or canAttackDiagonally(currPawn, dirPawn, pawns)
+      return canAttackHorizontally(currPawn, dirPawn, state) or canAttackVertically(currPawn, dirPawn, state) or canAttackDiagonally(currPawn, dirPawn, state)
     if (currPawn['type'] == "KNIGHT"):
       return (abs(currPawn["row"]-dirPawn["row"])==2 and abs(currPawn["col"]-dirPawn["col"])==1) or (abs(currPawn["row"]-dirPawn["row"])==1 and abs(currPawn["col"]-dirPawn["col"])==2)
     if (currPawn['type'] == "BISHOP"):
-      return canAttackDiagonally(currPawn, dirPawn, pawns)
+      return canAttackDiagonally(currPawn, dirPawn, state)
     if (currPawn['type'] == "ROOK"):
-      return canAttackHorizontally(currPawn, dirPawn, pawns) or canAttackVertically(currPawn, dirPawn, pawns)
+      return canAttackHorizontally(currPawn, dirPawn, state) or canAttackVertically(currPawn, dirPawn, state)
 
 # Check whether the current pawn can attack the other pawn (horizontally)
-def canAttackHorizontally(currPawn,dirPawn, pawns):
+def canAttackHorizontally(currPawn,dirPawn, state):
   if (currPawn["row"] == dirPawn["row"]):
     # Check whether there's another pawn between them
-    for pawn in pawns: 
+    for pawn in state: 
       if (pawn["row"] == currPawn["row"]):
         if (currPawn["col"] < dirPawn["col"]) and (currPawn["col"] < pawn["col"]) and (pawn["col"] < dirPawn["col"]):
           return False
@@ -143,10 +140,10 @@ def canAttackHorizontally(currPawn,dirPawn, pawns):
     return False
 
 # Check whether the current pawn can attack the other pawn (vertically)
-def canAttackVertically(currPawn, dirPawn, pawns):
+def canAttackVertically(currPawn, dirPawn, state):
   if (currPawn["col"] == dirPawn["col"]):
     # Check whether there's another pawn between them
-    for pawn in pawns: 
+    for pawn in state: 
       if (pawn["col"] == currPawn["col"]):
         if (currPawn["row"] < dirPawn["row"]) and (currPawn["row"] < pawn["row"]) and (pawn["row"] < dirPawn["row"]):
           return False
@@ -157,8 +154,8 @@ def canAttackVertically(currPawn, dirPawn, pawns):
     return False
 
 # Check whether the current pawn can attack the other pawn (diagonally)
-def canAttackDiagonally(currPawn, dirPawn, pawns):
-  if (abs(currPawn["col"] - dirPawn["col"]) == abs(currPawn["row"] - dirPawn["row"])): # both pawns is in the same diagonal
+def canAttackDiagonally(currPawn, dirPawn, state):
+  if (abs(currPawn["col"] - dirPawn["col"]) == abs(currPawn["row"] - dirPawn["row"])): # both state is in the same diagonal
     hgrad = 1 if (currPawn["col"] < dirPawn["col"]) else -1
     vgrad = 1 if (currPawn["row"] < dirPawn["row"]) else -1
   
@@ -167,7 +164,7 @@ def canAttackDiagonally(currPawn, dirPawn, pawns):
 
     while (x != dirPawn["row"] and y != dirPawn["col"] and x > 0 and y > 0):
       # check whether there's an obstacle in that cell
-      for pawn in pawns:
+      for pawn in state:
         if (pawn["row"] == x and pawn["col"] == y):
           return False
       x += vgrad
@@ -185,137 +182,107 @@ def isEnemy(pawn, dirPawn):
     return False
 
 # Count how many chess piece that can attack another piece
-def evaluate(pawns, numberOfPawns):
+def evaluate(state, pawnAmountBW):
   canAttackEnemy = 0
   canAttackFriend = 0
-  for pawn in pawns:
-    for dirPawn in pawns:
-      if checkAttack(pawn, dirPawn, pawns) and pawn!=dirPawn:
+  for pawn in state:
+    for dirPawn in state:
+      if checkAttack(pawn, dirPawn, state) and pawn!=dirPawn:
         if (isEnemy(pawn, dirPawn)):
           canAttackEnemy += 1
         else: # the other piece is not an enemy
           canAttackFriend += 1
-  if(numberOfPawns['BLACK'] == 0 or numberOfPawns['WHITE'] == 0):
+  if(pawnAmountBW['BLACK'] == 0 or pawnAmountBW['WHITE'] == 0):
     return canAttackFriend
   else:
-    return canAttackFriend + (2*numberOfPawns['BLACK']*numberOfPawns['WHITE'] - canAttackEnemy)
+    return canAttackFriend + (2*pawnAmountBW['BLACK']*pawnAmountBW['WHITE'] - canAttackEnemy)
 
 # check if a cell is not occupied by a pawn
-def notOccupied(pawns, x, y):
-  for pawn in pawns:
+def notOccupied(state, x, y):
+  for pawn in state:
     if (pawn['row'] == x and pawn['col'] == y):
       return False
   return True
 
 # return all neighbour from a given state
-def listAllNeighbour(pawns):
+def listAllNeighbour(state):
   stateList = []
-  for idx, val in enumerate(pawns):
+  for idx, val in enumerate(state):
     for x in range(8):
       for y in range(8):
-        if notOccupied(pawns, x, y):
-          neighbourState = copy.deepcopy(pawns)
+        if notOccupied(state, x, y):
+          neighbourState = copy.deepcopy(state)
           neighbourState[idx]['row'] = x
           neighbourState[idx]['col'] = y
           stateList.append(neighbourState)            
   return stateList
 
 # hill climbing function without color constraint
-def hillClimbing(initState, numberOfPawns):
+def hillClimbing(initState, pawnAmountBW):
   current = initState
-  evalCurrent = evaluate(current, numberOfPawns)
+  evalCurrent = evaluate(current, pawnAmountBW)
   isLocalMinim = False
   while (evalCurrent != 0 and not isLocalMinim):
     isLocalMinim = True
     AllNeighbour = listAllNeighbour(current)
     for neighbour in AllNeighbour:
-      if (evalCurrent > evaluate(neighbour, numberOfPawns)):
+      if (evalCurrent > evaluate(neighbour, pawnAmountBW)):
         isLocalMinim = False
         current = neighbour
-        evalCurrent = evaluate(neighbour, numberOfPawns)
+        evalCurrent = evaluate(neighbour, pawnAmountBW)
   return current
 
 def decision(probability):
   return random.randrange(100) < probability
 
 # Simulated Annealing function, temperature decreasing by ratio
-def simulatedAnnealing(initState,numberOfPawns,temperature,decreaseRate,iteration):
+def simulatedAnnealing(initState,pawnAmountBW,temperature,decreaseRate,iteration):
   current = initState
-  evalCurrent = evaluate(current,numberOfPawns)
+  evalCurrent = evaluate(current,pawnAmountBW)
   i = 0
-  isOver = False
-  while (evalCurrent != 0 and i < iteration and not isOver):
-    isOver = True
+  isLocalMinim = False
+  while (evalCurrent != 0 and i < iteration and not isLocalMinim):
+    isLocalMinim = True
     AllNeighbour = listAllNeighbour(current)
     for neighbour in AllNeighbour:
-      if (evalCurrent > evaluate(neighbour,numberOfPawns)):
+      if (evalCurrent > evaluate(neighbour,pawnAmountBW)):
         current = neighbour
-        evalCurrent = evaluate(current,numberOfPawns)
+        evalCurrent = evaluate(current,pawnAmountBW)
         i+=1
         temperature *= decreaseRate/100.00
-        isOver = False
+        isLocalMinim = False
       elif(temperature!=0):
-        probability = int(exp(evaluate(neighbour,numberOfPawns)-evalCurrent/temperature))
+        probability = int(exp(evaluate(neighbour,pawnAmountBW)-evalCurrent/temperature))
         if (decision(probability)):
           current = neighbour
-          evalCurrent = evaluate(current,numberOfPawns)
+          evalCurrent = evaluate(current,pawnAmountBW)
           i+=1
           temperature *= decreaseRate/100
-          isOver = False
-  return current
-
-def simulatedAnnealing2(initState,numberOfPawns,temperature,decreaseRate,iteration):
-  current = initState
-  evalCurrent = evaluate(current,numberOfPawns)
-  i = 0
-  while (evalCurrent != 0 and i < iteration):
-    neighbour = randomMove(current)
-    if (evalCurrent > evaluate(neighbour,numberOfPawns)):
-      current = neighbour
-      evalCurrent = evaluate(current,numberOfPawns)
-      temperature *= decreaseRate/100.00
-    elif(temperature!=0):
-      probability = int(exp(evaluate(neighbour,numberOfPawns)-evalCurrent/temperature))
-      if (decision(probability)):
-        current = neighbour
-        evalCurrent = evaluate(current,numberOfPawns)
-        temperature *= decreaseRate/100
-    i+=1
+          isLocalMinim = False
   return current
 
 #Metode penyelesaian menggunakan genetic algorithm
-def geneticAlgoritm(listOfPawns,numberOfPawns,jumlahPopulasi,limit):
-  listOfPawns = fitness(listOfPawns,numberOfPawns,jumlahPopulasi)
+def geneticAlgoritm(listOfstate,pawnAmountBW,jumlahPopulasi,limit):
+  listOfstate = fitness(listOfstate,pawnAmountBW,jumlahPopulasi)
   i = 0
-  while ( (evaluate(listOfPawns[0],numberOfPawns) != 0) and i < limit):
-    jumlahCrossOver = int(len(listOfPawns)/2)
+  while ( (evaluate(listOfstate[0],pawnAmountBW) != 0) and i < limit):
+    jumlahCrossOver = int(len(listOfstate)/2)
     for j in range (0,jumlahCrossOver):
-      anakAnak = crossOver(listOfPawns[2*j],listOfPawns[2*j+1])
+      anakAnak = crossOver(listOfstate[2*j],listOfstate[2*j+1])
       for anak in anakAnak:
         mutation(anak)
-        listOfPawns.append(anak)
-    listOfPawns = fitness(listOfPawns,numberOfPawns,jumlahPopulasi)
+        listOfstate.append(anak)
+    listOfstate = fitness(listOfstate,pawnAmountBW,jumlahPopulasi)
     i+=1
-  return listOfPawns
+  return listOfstate[0]
   
 # crossover setengah dari anak
 def crossOver(state1, state2):
-  # anakAnak = []
-  # anak1 = []
-  # anak2 = []
   if (len(state1) % 2 == 0):
-    # anak1.append(state1[0:int(len(state1)/2)])
-    # anak1.append(state2[(int(len(state1)/2) + 1):len(state2)])
-    # anak2.append(state2[0:int(len(state1)/2)])
-    # anak2.append(state1[(int(len(state1)/2) + 1):len(state2)])
     anak1 = state1[0:int(len(state1)/2)] + state2[int(len(state1)/2):len(state2)]
     anak2 = state2[0:int(len(state1)/2)] + state1[int(len(state1)/2):len(state2)]
 
   else:
-    # anak1.append(state1[0:(int(len(state1)/2)+1)])
-    # anak1.append(state2[(int(len(state1)/2) + 2):len(state2)])
-    # anak2.append(state2[0:(int(len(state1)/2)+1)])
-    # anak2.append(state1[(int(len(state1)/2) + 2):len(state2)])
     anak1 = state1[0:(int(len(state1)/2)+1)] + state2[(int(len(state1)/2) + 1):len(state2)]
     anak2 = state2[0:(int(len(state1)/2)+1)] + state1[(int(len(state1)/2) + 1):len(state2)]
   anakAnak = [anak1, anak2]
@@ -333,11 +300,11 @@ def mutation(state):
   state[i]['col'] = y
 
 #Fitness Function
-def fitness(listOfState, numberOfPawns, jumlahPopulasi):
+def fitness(listOfState, pawnAmountBW, jumlahPopulasi):
   hasil=[]
   listConnected=[]
   for idx,val in enumerate(listOfState):
-    listConnected.append((idx,evaluate(val,numberOfPawns)))
+    listConnected.append((idx,evaluate(val,pawnAmountBW)))
   listConnected = sorted(listConnected,key=itemgetter(1))
   for idx,val in listConnected:
     hasil.append(listOfState[idx])
@@ -352,22 +319,4 @@ def removeDuplicate(listState):
       hasil.append(value)
   return hasil
 
-# random move
-def randomMove(state):
-  neighbour = state
-  i = random.randrange(0,len(state))
-  x = random.randrange(0,8)
-  y = random.randrange(0,8)
-  while not notOccupied(state,x,y):
-    x = random.randrange(0,8)
-    y = random.randrange(0,8)
-  neighbour[i]['row'] = x
-  neighbour[i]['col'] = y
-  return neighbour 
-
-pawns=[]
-numberOfPawns = {}
-numberOfPawns['WHITE'] = 0
-numberOfPawns['BLACK'] = 0
-dataSplitted = []
-menuInit(pawns,numberOfPawns)
+menuInit()
